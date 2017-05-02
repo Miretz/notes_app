f098db15f8c2a0c50f71234c0f32ab5542d23c38
Miroslav Semerad <miretz@gmail.com>
2017-05-02 11:31:43 +0200

    Small refactor of main.js, adding week separators

--- js/main.js
+++ js/main.js
@@ -13,13 +13,14 @@ var WEEKDAYS = [
   "Saturday"
 ];
 
-function getDate(offset){
+
+function getDate(offset) {
   var d = new Date();
   d.setDate(d.getDate() + offset);
   var n = WEEKDAYS[d.getDay()];
-  var weekend = d.getDay() == 6 || d.getDay() == 0;
+  var weekend = d.getDay() === 6 || d.getDay() === 0;
   return {
-    "id": "note_" + d.toISOString().slice(0,10),
+    "id": "note_" + d.toISOString().slice(0, 10),
     "date": d,
     "weekend": weekend,
     "text": d.toLocaleDateString("sk") + ' - ' + n,
@@ -28,109 +29,159 @@ function getDate(offset){
   };
 }
 
-function createSection(date){
-  var panel_class = date.weekend? "panel-default" : "panel-info";
-  panel_class = date.is_current? "panel-primary" : panel_class;
 
-  var month_header = '';
-  if(date.start_of_month){
-    month_header = '<hr /><h2>'+ MONTHS[date.date.getMonth()] +'</h2>';
+function getWeekOfYear(date) {
+  var onejan = new Date(new Date().getFullYear(), 0, 1);
+  var dayOfYear = ((date.date - onejan + 1) / 86400000);
+  return Math.ceil(dayOfYear / 7);
+}
+
+
+function getNoteHeader(date) {
+  var header = '';
+  if (date.start_of_month) {
+    header = '<hr /><h2>' + MONTHS[date.date.getMonth()] + '</h2>';
   }
+  if (date.date.getDay() === 1) {
+    header = '<hr /><p>Week ' + getWeekOfYear(date) + '</p>';
+  }
+}
 
+
+function getPanelClass(date) {
+  var panel_class = date.weekend ? "panel-default" : "panel-info";
+  panel_class = date.is_current ? "panel-primary" : panel_class;
+  return panel_class;
+}
+
+
+function createSection(date) {
+
+  var panel_class = getPanelClass(date);
+  var header = getNoteHeader(date);
+
+  //collapse weekend notes
+  var collapse = date.weekend ? '' : 'in';
   var section = [
-    month_header,
-    '<div class="panel '+ panel_class +'" id="' + date.id + '">',
+    header,
+    '<div class="panel ' + panel_class + '" id="' + date.id + '">',
     '<div class="panel-heading">',
     '<h3 class="panel-title">',
     '<a data-toggle="collapse" href="#collapse_' + date.id + '">',
     '' + date.text + '</a></h3>',
     '</div>',
-    '<div id="collapse_' + date.id + '" class="panel-collapse collapse in">',
+    '<div id="collapse_' + date.id + '" class="panel-collapse collapse ' + collapse + '">',
     '<div class="panel-body">',
     '<textarea class="form-control no-border" id="text_' + date.id + '"',
     'style="min-width: 100%"></textarea>',
     '</div>',
     '</div>',
-    '</div>'];
+    '</div>'
+  ];
   section = $(section.join(''));
   return section;
 }
 
-function scrollTo(days, offset_top, offset_bottom){
+
+function initializeLinks() {
+  $('#today-link').click(function(e) {
+    scrollTo(0);
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  $('#lw-link').click(function(e) {
+    scrollTo(-7);
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  $('#nw-link').click(function(e) {
+    scrollTo(7);
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  $('#lm-link').click(function(e) {
+    scrollTo(-30);
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  $('#nm-link').click(function(e) {
+    scrollTo(30);
+    e.stopPropagation();
+    e.preventDefault();
+  });
+}
+
+
+function scrollTo(days) {
 
   //if it does not exist than generate
-  if(days > offset_bottom){
-    for(var i = offset_bottom; i <= days + 5; i++){
-      offset_bottom = offset_bottom + 1;
-      $('#container').append(createSection(getDate(offset_bottom)));
+  var i = 0;
+  if (days > window.offset_bottom) {
+    for (i = window.offset_bottom; i <= days + 5; i++) {
+      addNoteBottom();
     }
-  }
-  if(days < offset_top){
-    for(var i = offset_top; i >= days - 5; i--){
-      offset_top = offset_top - 1;
-      $('#container').prepend(createSection(getDate(offset_top)));
+  } else if (days < offset_top) {
+    for (i = window.offset_top; i >= days - 5; i--) {
+      addNoteTop();
     }
   }
 
   $('html, body').animate({
-        scrollTop: $("#" + getDate(days).id).offset().top - 120
+    scrollTop: $("#" + getDate(days).id).offset().top - 120
   }, 1000);
 }
 
 
-$( document ).ready(function() {
+function addNoteBottom() {
+  window.offset_bottom = window.offset_bottom + 1;
+  $('#container').append(createSection(getDate(window.offset_bottom)));
+}
 
-  var offset_top = -10;
-  var offset_bottom = 10;
 
-  //initialization
-  for(var i = offset_top; i < offset_bottom; i++){
+function addNoteTop() {
+  window.offset_top = window.offset_top - 1;
+  $('#container').prepend(createSection(getDate(window.offset_top)));
+}
+
+
+function initializeNotes() {
+  for (var i = window.offset_top; i < window.offset_bottom; i++) {
     var date = getDate(i);
     $('#container').append(createSection(date));
   }
+}
 
-  //center on current date
-  $('#today-link').click(function(e) {
-     scrollTo(0, offset_top, offset_bottom);
-       e.stopPropagation(); e.preventDefault();
-  });
-  $('#lw-link').click(function(e) {
-     scrollTo(-7, offset_top, offset_bottom);
-       e.stopPropagation(); e.preventDefault();
-  });
-  $('#nw-link').click(function(e) {
-     scrollTo(7, offset_top, offset_bottom);
-       e.stopPropagation(); e.preventDefault();
-  });
-  $('#lm-link').click(function(e) {
-     scrollTo(-30, offset_top, offset_bottom);
-       e.stopPropagation(); e.preventDefault();
-  });
-  $('#nm-link').click(function(e) {
-     scrollTo(30, offset_top, offset_bottom);
-       e.stopPropagation(); e.preventDefault();
-  });
 
-  //infinite scrolling
-  var previousScroll = $(window).scrollTop();
-  var scrollListener = function () {
-      $(window).bind("scroll", function () {
-          if ($(window).scrollTop() >= $(document).height() - $(window).height() - 100) {
-            offset_bottom = offset_bottom + 1;
-            $('#container').append(createSection(getDate(offset_bottom)));
-          }
-          if ($(window).scrollTop()!==previousScroll && $(window).scrollTop() <= 100) {
-            offset_top = offset_top - 1;
-            var pos = $('#container').first().offset().top;
-            $('#container').prepend(createSection(getDate(offset_top)));
-            $('html, body').scrollTop(pos + 100);
-            previousScroll = $(window).scrollTop();
-          }
-      });
-  };
+function scrollListener() {
+  $(window).bind("scroll", function() {
+    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 100) {
+      addNoteBottom();
+    }
+    if ($(window).scrollTop() !== window.previousScroll && $(window).scrollTop() <= 100) {
+      var pos = $('#container').first().offset().top;
+      addNoteTop();
+      $('html, body').scrollTop(pos + 100);
+      window.previousScroll = $(window).scrollTop();
+    }
+  });
+}
+
+
+$(document).ready(function() {
+
+  window.offset_top = -10;
+  window.offset_bottom = 10;
+
+  //initialization
+  initializeNotes();
+  initializeLinks();
+
+  window.previousScroll = $(window).scrollTop();
 
   //scroll to the current day and start the listener
   $('html, body').animate({
-        scrollTop: $("#" + getDate(0).id).offset().top - 200
-  }, 1000, function(){scrollListener()});
+    scrollTop: $("#" + getDate(0).id).offset().top - 200
+  }, 1000, function() {
+    scrollListener();
+  });
 });
